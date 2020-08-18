e92e0bf6730fc6ac681aeab1a4f5a0ce4ac1764e
Mohammad Dwairi <49045447+Mohammad-Dwairi@users.noreply.github.com>
2020-08-18 17:55:31 +0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -51,6 +51,6 @@ def send_control(steering_angle, throttle):
 
 
 if __name__ == '__main__':
-    model = load_model('model6.h5')
+    model = load_model('model.h5')
     app = socketio.Middleware(sio, app)
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
